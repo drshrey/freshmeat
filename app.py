d173eb4e4ca48d46a257fd046cf1bbec3d449bb1
sysod <drshrey@nbp-166-230.nbp.ruw.rutgers.edu>
2015-12-08 13:35:00 -0500

    css formatting and random entries implemented:

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        time.sleep(5)
+        time.sleep(60)
         # Make random entry 
         from random_entries import create_entries
         import fn
@@ -67,25 +67,23 @@ def background_thread():
         import random
         import mysql.connector
 
-        cxn = mysql.connector.connect(user=AWS_MYSQL_USER, password=AWS_MYSQL_PASSWORD, 
-            host='freshmeat.c3ne5kfmg1jo.us-west-2.rds.amazonaws.com', database='animals')
-        cursor = cxn.cursor()
-        query = ("select * from murder order by RAND() limit 1")
-        cursor.execute(query)
-        randRow = None
-        for i in cursor:
-            randRow = list(i)
-        '''
+        #cxn = mysql.connector.connect(user=AWS_MYSQL_USER, password=AWS_MYSQL_PASSWORD, 
+            #host='freshmeat.c3ne5kfmg1jo.us-west-2.rds.amazonaws.com', database='animals')
+        #cursor = cxn.cursor()
+        #query = ("select * from murder order by RAND() limit 1")
+        #cursor.execute(query)
+        #for i in cursor:
+        #    randRow = list(i)
         row = create_entries(1, today=True)[0]
+        print row
         newMurder = Murder.create(animal=row[0], quantity=row[1], body_part_found=row[2],
             date_started=row[3], date_closed=row[4], source=row[5], division=row[6],
             form=row[7], status=row[8], priority=row[9], location=row[10],
             complaint_type=row[11], resolution=row[12])
         newMurder.save()
-        '''
-        print randRow
+        row.insert(0, "")
         socketio.emit('murder',
-                      {'data': randRow},
+                      {'data': row},
                       namespace='/test')
 
 @app.route('/')
