315788be1277d7ef9dade0d1211142e247dc9cc3
sysod <drshrey@nbp-166-230.nbp.ruw.rutgers.edu>
2015-12-08 12:46:30 -0500

    INITIAL

---
+++ app.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on available packages.
+async_mode = None
+AWS_MYSQL_USER = ""
+AWS_MYSQL_PASSWORD = ""
+
+with open('creds.txt') as f:
+    credentials = [x.strip().split(':') for x in f.readlines()]
+    for username, password in credentials:
+        AWS_MYSQL_USER = str(username)
+        AWS_MYSQL_PASSWORD = str(password)
+
+print AWS_MYSQL_USER
+print AWS_MYSQL_PASSWORD
+
+if async_mode is None:
+    try:
+        import eventlet
+        async_mode = 'eventlet'
+    except ImportError:
+        pass
+
+    if async_mode is None:
+        try:
+            from gevent import monkey
+            async_mode = 'gevent'
+        except ImportError:
+            pass
+
+    if async_mode is None:
+        async_mode = 'threading'
+
+    print('async_mode is ' + async_mode)
+
+# monkey patching is necessary because this application uses a background
+# thread
+if async_mode == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+elif async_mode == 'gevent':
+    from gevent import monkey
+    monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        time.sleep(5)
+        # Make random entry 
+        from random_entries import create_entries
+        import fn
+        from db_init import Murder
+        import random
+        import mysql.connector
+
+        cxn = mysql.connector.connect(user=AWS_MYSQL_USER, password=AWS_MYSQL_PASSWORD, 
+            host='freshmeat.c3ne5kfmg1jo.us-west-2.rds.amazonaws.com', database='animals')
+        cursor = cxn.cursor()
+        query = ("select * from murder order by RAND() limit 1")
+        cursor.execute(query)
+        randRow = None
+        for i in cursor:
+            randRow = list(i)
+        '''
+        row = create_entries(1, today=True)[0]
+        newMurder = Murder.create(animal=row[0], quantity=row[1], body_part_found=row[2],
+            date_started=row[3], date_closed=row[4], source=row[5], division=row[6],
+            form=row[7], status=row[8], priority=row[9], location=row[10],
+            complaint_type=row[11], resolution=row[12])
+        newMurder.save()
+        '''
+        print randRow
+        socketio.emit('murder',
+                      {'data': randRow},
+                      namespace='/test')
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.daemon = True
+        thread.start()
+    return render_template('index.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/subscribe')
+def subscribe():
+    return render_template('subscribe.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ random_entries.py
@@ -0,0 +1,101 @@
+import csv
+import random
+
+# Globals
+ANIMALS = ["Birds", "Rooster", "Lamb", "Dove&Chicken", "Pigeon", "Pigeon&Pig", "Not Specified", "Rat",
+"Goat","Goat&Rooster&Lamb"]
+BODY_PART_FOUND = ["head", "legs", "head and body", "body without head"]
+DIVISION = ["Manhattan", "Brooklyn", "Bronx", "Queens"]
+PRIORITY = ["Normal", "High", "Low"]
+RESOLUTION = ["The Department of Parks and Recreation has completed the requested work order and corrected the problem.",
+			  "No action was taken because the Department of Parks and Recreation determined that the issue reported was out of its jurisdiction.",
+			  "The condition was determined to be an issue appropriate for handling by an alternate entity. The Department of Parks and Recreation has notified the appropriate resource."]
+
+
+
+def create_entries(numTimes, today=False):
+	entries = []
+	for i in xrange(numTimes):
+		animal = random.choice(ANIMALS)
+		quantity = random.choice(range(1,6))
+		bodyPartFound = random.choice(BODY_PART_FOUND)
+		
+		# Create random start date
+		import datetime
+		startDate = None
+		dateClosed = None
+		if today == False:
+			day = random.choice(range(1,26))
+			month = random.choice(range(1,13))
+			year = random.choice(range(1990, 2016))
+			startDate = datetime.datetime(year, month, day)
+			dateClosed = datetime(year, month, random.choice(range(day, day + 3)))
+		else:
+			now = datetime.datetime.utcnow()
+			day = now.day - 1
+			year = now.year
+			month = now.month
+			import math
+			hour = random.choice(range(1, 12))
+			minutes = random.choice(range(1,60))
+			seconds = random.choice(range(1,60))
+			startDate = datetime.datetime(year, month, day, hour, minutes, seconds)
+			dateClosed = datetime.datetime(year, month, day, random.choice(range(hour+1, hour + 5)), minutes, seconds)
+		
+		# end date		
+		source = "3-1-1 Call Center"
+		division = random.choice(DIVISION)
+		form = "DPR General Form"
+		status = "Closed"
+		priority = random.choice(PRIORITY)
+		location = get_google_maps_location(get_random_location(division))
+		complaintType = "Animal in a Park"
+		resolution = random.choice(RESOLUTION)
+		entry = [animal, quantity, bodyPartFound, startDate.strftime("%m/%d/%Y %I:%M:%S"), dateClosed.strftime("%m/%d/%Y %I:%M:%S"), source, "Borough Maintenance Operations Office - " + division, form, status, priority, location, complaintType, resolution]
+		entries.append(entry)
+	return entries
+
+def drange(start, stop, step):
+	r = start
+	while r < stop:
+		yield r
+		r += step
+
+def get_random_location(division):
+	import numpy as np
+	# Brooklyn : 40.649848 +- .01, -73.945694 +- 0.01
+	# Queens : 40.692597 +- 0.01, -73.806657 +- 0.01
+	# Manhattan : 40.776462 +- 0.01, -73.971637 +- 0.005
+	# Bronx : 40.859689 +- 0.005, -73.889345 +- 0.005
+	location = None
+	lat = None
+	lon = None
+	if(division == "Brooklyn"):
+		lat = random.choice(np.arange(40.639848, 40.659848, 0.000001))
+		lon = -1 * random.choice(np.arange(73.935694, 73.955694, 0.000001))
+	if(division == "Queens"):
+		lat = random.choice(np.arange(40.682597, 40.699848, 0.000001))
+		lon = -1 * random.choice(np.arange(73.79999, 73.81000, 0.000001))
+	if(division == "Manhattan"):
+		lat = random.choice(np.arange(40.766462, 40.786462, 0.000001))
+		lon = -1 * random.choice(np.arange(73.961637, 73.981637, 0.000001))
+	if(division == "Bronx"):
+		lat = random.choice(np.arange(40.849689, 40.859689, 0.000001))
+		lon = -1 * random.choice(np.arange(73.879345, 73.889345, 0.000001))
+	return (lat, lon)
+
+def get_google_maps_location(latLonTuple):
+	import requests
+	urlstring = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(latLonTuple[0]) + "," + str(latLonTuple[1]) + "&key=AIzaSyBBZu8Gbp9lyVXhO_sBB51wWYZLm_-sCes" 
+	formatted_address = requests.get(urlstring).json()['results'][0]['formatted_address']
+	print str(latLonTuple) + "-->" + formatted_address + "\n"
+	return formatted_address
+
+
+if __name__ == '__main__':
+	entries = create_entries(1000)
+	with open('test_incidents.csv', 'wb') as ti:
+		writer = csv.writer(ti, delimiter=',')
+		for entry in entries:
+			writer.writerow(entry)
+			
